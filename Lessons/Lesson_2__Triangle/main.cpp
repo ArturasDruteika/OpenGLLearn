85895efab5a38a6e2ef50a7b288770298e3877fd
Artūras Drūteika <arturas.druteika@gmail.com>
2026-03-07 11:15:11 +0200

    Adds a simple triangle example

--- Lessons/Lesson_2__Triangle/main.cpp
+++ Lessons/Lesson_2__Triangle/main.cpp
@@ -15,6 +15,59 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+int CompileShader(const char* source, unsigned int shaderID)
+{
+    glShaderSource(shaderID, 1, &source, nullptr);
+    glCompileShader(shaderID);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(shaderID, 512, nullptr, infoLog);
+        spdlog::error("Shader compilation failed: {}", infoLog);
+    }
+    return success;
+}
+
+unsigned int CreateShaderProgram(const char* vertexSource, const char* fragmentSource)
+{
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    if (!CompileShader(vertexSource, vertexShader))
+    {
+        glDeleteShader(vertexShader);
+        return 0;
+    }
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    if (!CompileShader(fragmentSource, fragmentShader))
+    {
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        return 0;
+    }
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        spdlog::error("Shader program linking failed: {}", infoLog);
+        shaderProgram = 0;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
 
 
 int main()
@@ -49,18 +102,67 @@ int main()
     }
 
     float triangleVertices[] = {
-        // positions
-         0.0f,  0.5f, 0.0f, // top vertex
-        -0.5f, -0.5f, 0.0f, // bottom left
-         0.5f, -0.5f, 0.0f  // bottom right
+        // positions         // colors
+         0.0f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f, 1.0f, // top vertex 
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 1.0f, 1.0f, // bottom left
+         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 1.0f, 1.0f  // bottom right
     };
+
+    unsigned int vbo;
+    unsigned int vao;
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    const char* vertexShaderSource = R"(
+        #version 460 core
+        layout (location = 0) in vec3 aPos;
+        layout (location = 1) in vec4 aColor;
+
+        out vec4 ourColor;
+
+        void main()
+        {
+            gl_Position = vec4(aPos, 1.0);
+            ourColor = aColor;
+        }
+    )";
+
+    const char* fragmentShaderSource = R"(
+        #version 460 core
+        in vec4 ourColor;
+        out vec4 FragColor;
+
+        void main()
+        {
+            FragColor = ourColor;
+        }
+    )";
+
+    unsigned int shaderProgram = CreateShaderProgram(vertexShaderSource, fragmentShaderSource);
+    if (shaderProgram == 0)
+    {
+        glfwDestroyWindow(pWindow);
+        glfwTerminate();
+        return -1;
+    }
 
     while (!glfwWindowShouldClose(pWindow))
     {
         glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(pWindow);
         glfwPollEvents();
