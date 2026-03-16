8eeca79a81bcaafe341756ae345ec95e76e5487c
Artūras Drūteika <arturas.druteika@gmail.com>
2026-03-16 19:00:19 +0200

    Add Orion Math library with linear algebra and trigonometry

---
+++ Orion/Math/Math/Trigonometry/include/Trigonometry.hpp
@@ -0,0 +1,22 @@
+// Myliu savo spoonchik spoonchik
+
+
+#ifndef MATH__TRIGONOMETRY__HPP
+#define MATH__TRIGONOMETRY__HPP
+
+
+#include "../../MacroExports/include/MacroExports.hpp"
+
+
+namespace Orion::Math
+{
+	class MATH_API Trigonometry
+	{
+	public:
+		static float Deg2Rad(float deg);
+		static float Rad2Deg(float rad);
+	};
+}
+
+
+#endif // MATH__TRIGONOMETRY__HPP
