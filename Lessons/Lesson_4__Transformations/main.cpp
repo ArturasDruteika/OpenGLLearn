21e874d3c70fdf928b6751f3c99fe66d13bc8f1f
Artūras Drūteika <arturas.druteika@gmail.com>
2026-03-17 18:27:12 +0200

    Refactor transformation functions to use Mat3 and update shader uniform handling

--- Lessons/Lesson_4__Transformations/main.cpp
+++ Lessons/Lesson_4__Transformations/main.cpp
@@ -5,6 +5,7 @@
 #include "Math/LinearAlgebra/include/LinearAlgebraOperations.hpp"
 #include "Math/Trigonometry/include/Trigonometry.hpp"
 #include "spdlog/spdlog.h"
+#include <cmath>
 #include <string>
 
 
@@ -75,9 +76,29 @@ unsigned int CreateShaderProgram(const std::string& vertexSource, const std::str
     return shaderProgram;
 }
 
-const float* GetMatrixData(const Orion::Math::Mat4& matrix)
+const float* GetMatrixData(const Orion::Math::Mat3& matrix)
 {
     return &matrix[0][0];
+}
+
+Orion::Math::Mat3 MakeTranslation2D(const Orion::Math::Vec2& translation)
+{
+    Orion::Math::Mat3 result{};
+    result[0][2] = translation[0];
+    result[1][2] = translation[1];
+    return result;
+}
+
+Orion::Math::Mat3 MakeRotation2D(float angleRadians)
+{
+    const float cosine = std::cos(angleRadians);
+    const float sine = std::sin(angleRadians);
+
+    return Orion::Math::Mat3{
+        Orion::Math::Vec3{ cosine, -sine, 0.0f },
+        Orion::Math::Vec3{ sine, cosine, 0.0f },
+        Orion::Math::Vec3{ 0.0f, 0.0f, 1.0f }
+    };
 }
 
 
@@ -140,13 +161,14 @@ int main()
         layout (location = 0) in vec2 aPos;
         layout (location = 1) in vec4 aColor;
 
-        uniform mat4 uTransform;
+        uniform mat3 u_transform;
 
         out vec4 ourColor;
 
         void main()
         {
-            gl_Position = uTransform * vec4(aPos, 0.0, 1.0);
+            vec3 transformed = u_transform * vec3(aPos, 1.0);
+            gl_Position = vec4(transformed.xy, 0.0, 1.0);
             ourColor = aColor;
         }
     )";
@@ -171,14 +193,25 @@ int main()
     }
 
     const float rotationAngle = Orion::Math::Trigonometry::Deg2Rad(90.0f);
-    const Orion::Math::Vec3 rotationAxis = Orion::Math::NEGATIVE_Z_AXIS;
-    const Orion::Math::Vec3 nonUniformScale{ 0.5f, 1.5f, 1.0f };
-    const Orion::Math::Vec3 topLeftPosition{ -0.5f, 0.5f, 0.0f };
-    const Orion::Math::Vec3 bottomLeftPosition{ -0.5f, -0.5f, 0.0f };
-    const Orion::Math::Vec3 topRightPosition{ 0.5f, 0.5f, 0.0f };
-    const Orion::Math::Vec3 bottomRightPosition{ 0.5f, -0.5f, 0.0f };
+    const Orion::Math::Vec2 nonUniformScale{ 0.5f, 1.5f };
+    const Orion::Math::Vec2 topLeftPosition{ -0.5f, 0.5f };
+    const Orion::Math::Vec2 bottomLeftPosition{ -0.5f, -0.5f };
+    const Orion::Math::Vec2 topRightPosition{ 0.5f, 0.5f };
+    const Orion::Math::Vec2 bottomRightPosition{ 0.5f, -0.5f };
 
-    int transformLocation = glGetUniformLocation(shaderProgram, "uTransform");
+    Orion::Math::Mat3 topLeft = MakeTranslation2D(topLeftPosition);
+
+    Orion::Math::Mat3 bottomLeft = MakeTranslation2D(bottomLeftPosition);
+    bottomLeft = bottomLeft * MakeRotation2D(rotationAngle);
+
+    Orion::Math::Mat3 topRight = MakeTranslation2D(topRightPosition);
+    topRight = Orion::Math::LinAlgOps::Scale(topRight, Orion::Math::Vec3{ nonUniformScale[0], nonUniformScale[1], 1.0f });
+
+    Orion::Math::Mat3 bottomRight = MakeTranslation2D(bottomRightPosition);
+    bottomRight = bottomRight * MakeRotation2D(rotationAngle);
+    bottomRight = Orion::Math::LinAlgOps::Scale(bottomRight, Orion::Math::Vec3{ nonUniformScale[0], nonUniformScale[1], 1.0f });
+
+    int transformLocation = glGetUniformLocation(shaderProgram, "u_transform");
     while (!glfwWindowShouldClose(pWindow))
     {
         glClearColor(BACKGROUND_COLOR[0], BACKGROUND_COLOR[1], BACKGROUND_COLOR[2], BACKGROUND_COLOR[3]);
@@ -187,24 +220,16 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(vao);
 
-        Orion::Math::Mat4 topLeft = Orion::Math::LinAlgOps::Translate(topLeftPosition);
-        glUniformMatrix4fv(transformLocation, 1, GL_TRUE, GetMatrixData(topLeft));
+        glUniformMatrix3fv(transformLocation, 1, GL_TRUE, GetMatrixData(topLeft));
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        Orion::Math::Mat4 bottomLeft = Orion::Math::LinAlgOps::Translate(Orion::Math::Mat4{}, bottomLeftPosition);
-        bottomLeft = Orion::Math::LinAlgOps::Rotate(bottomLeft, rotationAngle, rotationAxis);
-        glUniformMatrix4fv(transformLocation, 1, GL_TRUE, GetMatrixData(bottomLeft));
+        glUniformMatrix3fv(transformLocation, 1, GL_TRUE, GetMatrixData(bottomLeft));
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        Orion::Math::Mat4 topRight = Orion::Math::LinAlgOps::Translate(Orion::Math::Mat4{}, topRightPosition);
-        topRight = Orion::Math::LinAlgOps::Scale(topRight, nonUniformScale);
-        glUniformMatrix4fv(transformLocation, 1, GL_TRUE, GetMatrixData(topRight));
+        glUniformMatrix3fv(transformLocation, 1, GL_TRUE, GetMatrixData(topRight));
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        Orion::Math::Mat4 bottomRight = Orion::Math::LinAlgOps::Translate(Orion::Math::Mat4{}, bottomRightPosition);
-        bottomRight = Orion::Math::LinAlgOps::Rotate(bottomRight, rotationAngle, rotationAxis);
-        bottomRight = Orion::Math::LinAlgOps::Scale(bottomRight, nonUniformScale);
-        glUniformMatrix4fv(transformLocation, 1, GL_TRUE, GetMatrixData(bottomRight));
+        glUniformMatrix3fv(transformLocation, 1, GL_TRUE, GetMatrixData(bottomRight));
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(pWindow);
